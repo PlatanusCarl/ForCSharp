7a206b69a084dcf3a898762316ff6653394b0637
Platanus <Platanus.carl@gmail.com>
2020-02-24 15:38:32 +0800

    2/24

---
+++ homework2/arrayope/Program.cs
@@ -0,0 +1,66 @@
+﻿/*
+2.编程求一个整数数组的最大值、最小值、平均值和所有数组元素的和。
+
+*/
+
+using System;
+
+namespace arrayope
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = {1,2,3,4,5,6,7,8,9,10};
+            Console.WriteLine("{0} {1} {2} {3} ",max(array),mini(array),average(array),sum(array));
+
+            Console.ReadKey();
+        }
+        static int max(int[] array)
+        {
+            int max = array[0];
+            foreach(int i in array)
+            {
+                if(i > max)
+                {
+                    max = i;
+                }
+            }
+            return max;
+        }
+
+        static int mini(int[] array)
+        {
+            int mini = array[0];
+            foreach(int i in array)
+            {
+                if(i < mini)
+                {
+                    mini = i;
+                }
+            }
+            return mini;
+        }
+        static int average(int[] array)
+        {
+            int average=array[0] ,count=0;
+            foreach(int i  in array)
+            {
+                average += i;
+                count++;
+            }
+            average /= count;
+            return average;
+        }
+
+        static int sum(int[] array)
+        {
+            int sum = array[0];
+            foreach(int i  in array)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+    }
+}
---
+++ homework2/findodd/Program.cs
@@ -0,0 +1,57 @@
+﻿/*
+1.编写程序输出用户指定数据的所有素数因子。
+
+
+*/
+using System;
+
+namespace arrayope
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入数据：\n");
+            string str = Console.ReadLine();
+            int num = check(str);
+            find(num);
+            Console.ReadKey();
+        }
+
+        static int check(string str)//可以写一个tryparse
+        { 
+
+            int num = 0;
+            try
+            {
+                num = Int32.Parse(str);
+            }
+            catch (System.Exception)
+            {
+                Console.WriteLine("请重新输入：\n");
+                str = Console.ReadLine();
+                num = check(str);
+            }
+            if(num < 2)
+            {
+                Console.WriteLine("请重新输入：\n");
+                str = Console.ReadLine();
+                num = check(str);
+            }
+            return num;
+        }
+
+        static void find(int num)
+        {
+            for(int i = 2; i <= num; i++)
+            {
+                while(num%i == 0)
+                {
+                    Console.WriteLine(i);
+                    num/=i;
+                }
+            }
+                   
+        }
+    }
+}
