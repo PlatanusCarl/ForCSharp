9f16376acf14da02300bb0d00335e38f25ce4abb
Platanus <Platanus.carl@gmail.com>
2020-03-23 00:15:39 +0800

    homework3 finished

--- homework3/geometryClass/Factory.cs
+++ homework3/geometryClass/Factory.cs
@@ -6,6 +6,30 @@ namespace GeometryShapes
 {
     class Factory
     {
-
+        public Shape creatShape(int typeNum)
+        {
+            Shape mshape = null;
+            switch (typeNum)
+            {
+                case 1:
+                    mshape = new Circle(randnum());
+                    break;
+                case 2:
+                    mshape = new Rectangle(randnum(), randnum());
+                    break;
+                case 3:
+                    mshape = new Triangle(randnum(), randnum(), randnum());
+                    while (!mshape.IsLegal())
+                        mshape = new Triangle(randnum(), randnum(), randnum());
+                    break;
+            }
+            return mshape;
+        }
+        static private int randnum()
+        {
+            Random r = new Random();
+            int r1 = r.Next(1,100);
+            return r1;
+        }
     }
 }
--- homework3/geometryClass/Program.cs
+++ homework3/geometryClass/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿/* 
+ * 1.编写面向对象程序实现长方形、正方形、三角形等形状的类。每个形状类都能计算面积、判断形状是否合法。 请尝试合理使用接口/抽象类、属性来实现。
+ * 2.随机创建10个形状对象，计算这些对象的面积之和。 尝试使用简单工厂设计模式来创建对象。
+ */
+using System;
 
 namespace GeometryShapes
 {
@@ -6,10 +10,15 @@ namespace GeometryShapes
     {
         static void Main(string[] args)
         {
-            Triangle triangle = new Triangle(3,4,5);
-            Rectangle rectangle = new Rectangle(3, 2);
-            Circle circle = new Circle(-1);
-            Console.WriteLine("{0} {1} {2}",triangle.Area,rectangle.Area,circle.Area);
+            Factory fac = new Factory();
+
+           for(int i = 0;i<10;i++)
+            {
+                Random r = new Random();
+                int r1 = r.Next(1,4);
+                Shape sh = fac.creatShape(r1);
+                Console.WriteLine($"第{i+1}个图形是{sh.GetType().Name}，面积为{sh.Area}");
+            }
             Console.ReadLine();
         }
     }
